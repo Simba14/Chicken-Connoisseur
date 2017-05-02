047cde6908c12735bb8014c5580b4831f4030ab1
alicespyglass <alicespyglass@gmail.com>
2017-05-02 16:01:01 +0100

    Add show method

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -31,4 +31,15 @@ context 'creating restaurants' do
     expect(page).to have_content 'KFC'
     expect(current_path).to eq '/restaurants'
   end
+
+  context 'viewing restaurants' do
+    let!(:kfc) { Restaurant.create(name: 'KFC') }
+
+    scenario 'lets a user view a restaurant' do
+      visit '/restaurants'
+      click_link 'KFC'
+      expect(page).to have_content 'KFC'
+      expect(current_path).to eq "/restaurants/#{kfc.id}"
+    end
+  end
 end
