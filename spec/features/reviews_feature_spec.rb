493c947ec0a8cdd6a6604caa260df1587cdda67c
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-05-05 11:23:36 +0100

    adds number of ratings to view

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -30,4 +30,14 @@ feature 'reviewing' do
     leave_review('it was peng', 5)
     expect(page).to have_content 'Average rating: 4'
   end
+
+  scenario 'displays the total number of ratings' do
+    click_link 'Sign out'
+    sign_up(email: 'sean@123.com')
+    leave_review('was alright I guess', 3)
+    click_link 'Sign out'
+    sign_up(email: 'pete@123.com')
+    leave_review('it was peng', 5)
+    expect(page).to have_content '2 ratings'
+  end
 end
