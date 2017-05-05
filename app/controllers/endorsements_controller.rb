d5db44ad3e0d75af1f53fa977568b1430d8179aa
emmpak <eiliev05@yahoo.com>
2017-05-05 12:22:48 +0100

    Add endorsements

--- app/controllers/endorsements_controller.rb
+++ app/controllers/endorsements_controller.rb
@@ -1,9 +1,13 @@
 class EndorsementsController < ApplicationController
 
+  def new
+    create
+  end
+
   def create
     @review = Review.find(params[:review_id])
     @review.endorsements.create
-    redirect_to restaurants_path
+    redirect_to "/restaurants/#{@review.restaurant_id}"
   end
 
 end
--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -2,12 +2,18 @@ require 'rails_helper'
 
 feature 'endorsing reviews' do
   before do
-    kfc = Restaurant.create('KFC')
-    kfc.reviews.create(ratings: 3, thoughts: 'It was an abomination')
+    sign_up
+    create_restaurant
+    click_link 'Review KFC'
+    fill_in 'Thoughts', with: 'It was an abomination'
+    select '3', from: 'Rating'
+    click_button "Leave Reviews"
+    # Restaurant.last.reviews.create(rating: 3, thoughts: 'It was an abomination')
   end
 
   scenario 'user can endorse a review, which updates the review endorsement count' do
     visit '/restaurants'
+    click_link 'KFC'
     click_link 'Endorse review'
     expect(page).to have_content('1 endorsement')
   end
